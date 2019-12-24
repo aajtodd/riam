cd7f05dac3492c504d31e9f1c43606a4bbb94024
Aaron J Todd <aajtodd@gmail.com>
2019-12-24 14:09:24 -0500

    Rearrange fields and make them public

--- src/policy.rs
+++ src/policy.rs
@@ -1,7 +1,8 @@
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
 /// Effect indicates whether a policy statement allows or denies access
-#[derive(Serialize, Deserialize, Eq, PartialEq, Debug)]
+#[derive(Serialize, Deserialize, Eq, PartialEq, Debug, Clone)]
 pub enum Effect {
     #[serde(rename = "allow")]
     Allow,
@@ -10,34 +11,47 @@ pub enum Effect {
 }
 
 /// Statement contains information about a single permission
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 pub struct Statement {
-    effect: Effect,
-    actions: Vec<String>,
-    resources: Vec<String>,
+    /// An optional statement id. This is used to differentiate statements e.g. "Grant read access to resource:xyz"
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub sid: Option<String>,
+
+    /// Allow or Deny the actions
+    pub effect: Effect,
+
+    /// One or more actions that apply to the resources
+    pub actions: Vec<String>,
+
+    /// The resources the statement applies to
+    pub resources: Vec<String>,
 }
 
 /// Policy represents an access control policy which is used to either grant or deny a
 /// principal (users/groups/roles/etc) actions on specific resources.
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 pub struct Policy {
+    /// The unique ID assigned to the policy
     #[serde(skip_serializing_if = "Option::is_none")]
-    sid: Option<String>,
-    principals: Vec<String>,
-    statements: Vec<Statement>,
+    pub id: Option<Uuid>,
+
+    /// The policy name (e.g. "FullAdminAccess")
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub name: Option<String>,
+
+    /// The body of the policy
+    pub statements: Vec<Statement>,
 }
 
 impl Policy {
     /// Check if the policy is (structurally) valid
     pub fn is_valid(&self) -> bool {
-        if self.principals.is_empty() || self.statements.is_empty() {
-            return false;
-        }
-
-        return !self
-            .statements
-            .iter()
-            .any(|x| x.actions.is_empty() || x.resources.is_empty());
+        // TODO - validate resource names and action names follow whatever grammar we define for them
+        return !(self.statements.is_empty()
+            || self
+                .statements
+                .iter()
+                .any(|x| x.actions.is_empty() || x.resources.is_empty()));
     }
 }
 
@@ -51,25 +65,31 @@ mod tests {
     }
 
     #[test]
-    fn policy_serialization_no_sid() {
+    fn statement_serialization_no_sid() {
         // sid should be left off serialized json when not set
-        let policy = Policy {
+        let statement = Statement {
             sid: None,
-            principals: Vec::new(),
-            statements: Vec::new(),
+            effect: Effect::Deny,
+            actions: Vec::new(),
+            resources: Vec::new(),
         };
 
         assert_tokens(
-            &policy,
+            &statement,
             &[
                 Token::Struct {
-                    name: "Policy",
-                    len: 2,
+                    name: "Statement",
+                    len: 3,
                 },
-                Token::Str("principals"),
+                Token::Str("effect"),
+                Token::UnitVariant {
+                    name: "Effect",
+                    variant: "deny",
+                },
+                Token::Str("actions"),
                 Token::Seq { len: Some(0) },
                 Token::SeqEnd,
-                Token::Str("statements"),
+                Token::Str("resources"),
                 Token::Seq { len: Some(0) },
                 Token::SeqEnd,
                 Token::StructEnd,
@@ -80,9 +100,10 @@ mod tests {
     #[test]
     fn policy_serialization() {
         let policy = Policy {
-            sid: Some("my policy".into()),
-            principals: vec_of_strings!["user:test"],
+            name: Some("my policy".into()),
+            id: None,
             statements: vec![Statement {
+                sid: Some("my statement".into()),
                 effect: Effect::Allow,
                 actions: vec_of_strings!["blog:list"],
                 resources: vec_of_strings!["resources:blog:123", "resources:blog:*"],
@@ -94,21 +115,20 @@ mod tests {
             &[
                 Token::Struct {
                     name: "Policy",
-                    len: 3,
+                    len: 2,
                 },
-                Token::Str("sid"), // ensure sid is omitted when not specified
+                Token::Str("name"),
                 Token::Some,
                 Token::Str("my policy"),
-                Token::Str("principals"),
-                Token::Seq { len: Some(1) },
-                Token::Str("user:test"),
-                Token::SeqEnd,
                 Token::Str("statements"),
                 Token::Seq { len: Some(1) },
                 Token::Struct {
                     name: "Statement",
-                    len: 3,
+                    len: 4,
                 },
+                Token::Str("sid"),
+                Token::Some,
+                Token::Str("my statement"),
                 Token::Str("effect"),
                 Token::UnitVariant {
                     name: "Effect",
@@ -133,17 +153,15 @@ mod tests {
     #[test]
     fn policy_is_valid() {
         let mut policy = Policy {
-            sid: None,
-            principals: Vec::new(),
+            name: None,
+            id: None,
             statements: Vec::new(),
         };
 
         assert_eq!(false, policy.is_valid());
 
-        policy.principals.push("user:test".into());
-        assert_eq!(false, policy.is_valid());
-
         let st1 = Statement {
+            sid: None,
             effect: Effect::Allow,
             actions: vec_of_strings!["blog:list"],
             resources: vec_of_strings!["resources:blog:123", "resources:blog:*"],
@@ -151,6 +169,7 @@ mod tests {
 
         // invalid statement
         let st2 = Statement {
+            sid: None,
             effect: Effect::Deny,
             actions: vec_of_strings!["account:list"],
             resources: Vec::new(),
